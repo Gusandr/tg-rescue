b4963dfce39347be22fcd16855c2505d0374bfff
Gusandr <125789471+Gusandr@users.noreply.github.com>
2025-07-22 22:13:19 +0300

    ..

--- build/generated/sqldelight/code/Users/main/me/gusandr/sqldelight/hockey/data/SelectBanTime.kt
+++
@@ -1,7 +0,0 @@
-package me.gusandr.sqldelight.hockey.`data`
-
-import kotlin.Long
-
-public data class SelectBanTime(
-  public val blockTime: Long?,
-)
--- build/generated/sqldelight/code/Users/main/me/gusandr/sqldelight/hockey/data/SelectMessageUser.kt
+++
@@ -1,7 +0,0 @@
-package me.gusandr.sqldelight.hockey.`data`
-
-import kotlin.String
-
-public data class SelectMessageUser(
-  public val messageIds: String?,
-)
